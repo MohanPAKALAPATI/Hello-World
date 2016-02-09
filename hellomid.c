084822d06818b6734f999b5ef9d37c9f64c6cba4
Mohan PAKALAPATI <mohancmg@gmail.com>
2016-02-09 12:52:35 +0530

    Remove Bug#2
    Remove Bug#2

--- hellomid.c
+++ hellomid.c
@@ -4,7 +4,6 @@ HelloMid()
           printf("Hello Mid!\n");
 		  printf("Hello Mid From Jenkins!\n");
 		  printf("Hello Mid2 From Jenkins2!\n");
-		  printf("Hello Mid2 From Jenkins2!\n") //new bug
 		  
 	  }
       
